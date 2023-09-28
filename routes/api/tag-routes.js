c967d52cafb1cf651b3d16abf795031ee9fd532f
Drinkmilk <141216114+lndvng@users.noreply.github.com>
2023-09-28 01:35:52 -0700

    updated product and tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,55 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{ model: Product }]
+  }).then((tagData) => {
+    res.json(tagData)
+  }).catch((err) => {
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, { include: [{ model: Product }] 
+  }).then((tagData) => {
+    res.json(tagData)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((tagData) => {
+    res.json(tagData)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((tagData) => {
+    res.json(tagData)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  console.log("hello")
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((tagData) => {
+    res.json(tagData)
+  })
 });
 
 module.exports = router;
